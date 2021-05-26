44fced7b85197386306489600097556e9d986036
UsamaWaqas <usamawaqas.m@gmail.com>
2021-05-26 17:35:35 +0500

    some change

---
+++ freeapp/src/dinner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+ function  dinner() {
+    return (
+        <div>
+            <h1> today we are serving  </h1>
+        </div>
+    )
+              
+        
+       
+
+ } 
+ export default dinner;
